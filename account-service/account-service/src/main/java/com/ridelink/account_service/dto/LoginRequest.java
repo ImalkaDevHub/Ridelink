700de4906ab5672a8914a6850dddd1717e3894a7
IT24102603 <IT24102603@my.sliit.lk>
2026-09-26 05:48:24 +0530

    Commit with MongoDB
    Commit with MongoDB

--- account-service/account-service/src/main/java/com/ridelink/account_service/dto/LoginRequest.java
+++ account-service/account-service/src/main/java/com/ridelink/account_service/dto/LoginRequest.java
@@ -24,4 +24,7 @@ public class LoginRequest {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- account-service/account-service/src/main/java/com/ridelink/account_service/model/Account.java
+++ account-service/account-service/src/main/java/com/ridelink/account_service/model/Account.java
@@ -2,6 +2,7 @@ package com.ridelink.account_service.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 @Document(collection = "accounts")
 public class Account {
